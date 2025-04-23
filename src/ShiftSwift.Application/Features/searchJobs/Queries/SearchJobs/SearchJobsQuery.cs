075a6dfa181891f46e3005057c935ebf0c7af447
Ahmed <ahmed.m.shehata82@gmail.com>
2025-04-23 04:59:34 -1200

    Add SearchJobs endpoint

---
+++ src/ShiftSwift.Application/Features/searchJobs/Queries/SearchJobs/SearchJobsQuery.cs
@@ -0,0 +1,17 @@
+﻿using ErrorOr;
+using MediatR;
+using ShiftSwift.Application.DTOs.member;
+using ShiftSwift.Shared.ApiBaseResponse;
+using ShiftSwift.Shared.paging;
+namespace ShiftSwift.Application.Features.searchJobs.Queries.SearchJobs
+{
+   public sealed record SearchJobsQuery : PaginatedRequest, IRequest<ErrorOr<ApiResponse<PaginatedResponse<SearchJobsResponse>>>>
+   {
+     public string? Search { get; init; }
+     public string? Location { get; init; }
+     public double? MaxDistanceKm { get; init; }
+     public decimal? MinSalary { get; init; }
+     public decimal? MaxSalary { get; init; }
+     public int JobTypeIdFilterValue { get; init; }
+   }
+}
